50024211c01309fb7a54d4c226c8248b2bff0657
Sergey <104456968+Solod-S@users.noreply.github.com>
2024-06-20 16:27:07 +0300

    add google callendar get refresh token logic
    add google callendar get refresh token logic

--- front/src/views/private/ProjectView/components/GoogleCalendar copy 3.js
+++ front/src/views/private/ProjectView/components/GoogleCalendar copy 3.js
@@ -4,10 +4,11 @@ import axios from "axios";
 
 const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
 const SERVER_URL = process.env.REACT_APP_SERVER_URL;
-const SCOPES = [
-  "https://www.googleapis.com/auth/calendar.readonly",
-  "https://www.googleapis.com/auth/tasks.readonly",
-];
+// const SCOPES = [
+//   "https://www.googleapis.com/auth/calendar.readonly",
+//   "https://www.googleapis.com/auth/tasks.readonly",
+// ];
+const SCOPES = "https://www.googleapis.com/auth/tasks.readonly";
 
 const GoogleCalendar = () => {
   const [events, setEvents] = useState([]);
@@ -15,69 +16,54 @@ const GoogleCalendar = () => {
 
   useEffect(() => {
     function start() {
-      gapi.client
-        .init({
-          clientId: CLIENT_ID,
-          scope: SCOPES.join(" "),
-        })
-        .then(() => {
-          const authInstance = gapi.auth2.getAuthInstance();
-          console.log("authInstance", authInstance);
-        })
-        .catch(error => {
-          console.error("Error initializing gapi client:", error);
-        });
+      gapi.client.init({
+        clientId: CLIENT_ID,
+        scope: SCOPES,
+      });
     }
-
     gapi.load("client:auth2", start);
   }, []);
 
   const handleAuthClick = async () => {
     try {
       const authInstance = gapi.auth2.getAuthInstance();
+      console.log(`authInstance`, authInstance);
       const response = await authInstance.grantOfflineAccess({
         prompt: "consent",
       });
       console.log("Authorization Code:", response.code);
 
       // Отправляем код авторизации на сервер для обмена на токены
-      const res = await axios.post(`${SERVER_URL}/exchange-code-to-token`, {
+      const res = await axios.post(`${SERVER_URL}/exchange_code`, {
         code: response.code,
       });
 
       const data = res.data;
       console.log("Tokens:", data);
       setIsAuthorized(true);
-      loadCalendarEvents();
+      // loadCalendarEvents();
     } catch (error) {
       console.error("Error exchanging code:", error);
     }
   };
 
-  const loadCalendarEvents = async () => {
-    try {
-      const response = await gapi.client.calendar.events.list({
+  const loadCalendarEvents = () => {
+    gapi.client.calendar.events
+      .list({
         calendarId: "primary",
         timeMin: new Date().toISOString(),
         showDeleted: false,
         singleEvents: true,
         maxResults: 10,
         orderBy: "startTime",
-      });
-
-      console.log("Full response:", response); // Добавьте логирование для отладки
-
-      if (response && response.result && response.result.items) {
+      })
+      .then(response => {
         const events = response.result.items;
         setEvents(events);
-      } else {
-        console.error("Empty or invalid response:", response);
-        // Можно предпринять дополнительные действия, например, повторить запрос или обработать ошибку
-      }
-    } catch (error) {
-      console.error("Error loading calendar events:", error);
-      // Обработка ошибки при загрузке событий календаря
-    }
+      })
+      .catch(error => {
+        console.error("Error loading calendar events:", error);
+      });
   };
 
   return (
